b2cadcd1461ffcce97cc98fb6a2b135576d8b1cf
Jeffrey  Graham <jeffgraham685@gmail.com>
2020-02-13 18:09:18 +0000

    Content update

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -11,7 +11,8 @@ const AboutContent = () => {
                <h3>Web Designer and <span>Developer</span></h3>
                <h4>Hi! My name's Jeff, and I'm a web designer and developer based in Belfast.</h4>
 
-               <p>I began my journey as a developer after struggling to find a career that was both creatively and technically challenging. Having worked in a number of different fields over the past few years, I found myself growing frustrated with the lack of diversity in my daily routine. As someone who is passionate about learning, being a self taught guitarist, cook and photographer, I quickly fell in love with web development as it allows me to be learning something new every day, while challenging me both creatively in design and technically in the development side.</p>
+               <p>I began my journey as a developer after struggling to find a career that was both creatively and technically challenging. Having worked in a number of different fields over the past few years, I found myself growing frustrated with the lack of diversity in my daily routine. As someone who is passionate about learning, being a self taught guitarist, cook and photographer, I quickly fell in love with web development as it allows me to be learning something new every day, while challenging me both creatively in design and technically in development.</p>
+               <p>My long term goal is to find a company that I can work for remotely while travelling for a few years before returning to Belfast. At present however, I am focused on refining my skills and learning as much as possible.</p>
                <p>I am currently interested in junior developer roles and freelance work.</p>
                 <Button text="Get CV"/>
             </div>
